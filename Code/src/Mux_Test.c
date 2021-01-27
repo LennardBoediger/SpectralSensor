92d030497d2c002eab072e6e55e8b797edd16e69
LennardBoediger <lennard-boediger@posteo.de>
2021-01-27 20:18:46 +0100

    spellcheck latex

---
+++ Code/src/Mux_Test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <wiringPi.h>
+#include <wiringPiI2C.h>
+
+int main(){
+	printf("Mux Test:\n");
+    int fd =  wiringPiI2CSetup(0x70);
+    if (fd == -1) {
+        printf("i2c failed");
+    }
+    printf("i2c-connected\n");
+    // Send byte 0
+    wiringPiI2CWrite(fd, 1);
+    printf("Done\n");
+    return 0;
+}
