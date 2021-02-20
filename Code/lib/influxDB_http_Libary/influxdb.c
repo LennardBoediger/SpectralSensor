68c374d1c3869f43dd8c6bc6af0634455550e602
Lennard <lennard-boediger@posteo.de>
2021-02-20 10:31:25 +0100

    fixes to influxdb-lib

--- Code/lib/influxDB_http_Libary/influxdb.c
+++ Code/lib/influxDB_http_Libary/influxdb.c
@@ -23,7 +23,8 @@
 #define BUFSIZE 8196
 #define DEBUG   0
 
-
+#define SUCSESS 1
+#define FAIL 0
 
 int pexit(char * msg)
 {
@@ -49,9 +50,7 @@ int writeToDatabase(char measurement[],int i2c_adrress, uint64_t measurment_time
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr(IP_ADDRESS);
     serv_addr.sin_port = htons(PORT);
-    if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) <0)
-        pexit("connect() failed");
-
+    
     //  Create InfluxDB line protocol body
     //  1. measurement Name
     //  2. tag = device identifier
@@ -68,40 +67,54 @@ int writeToDatabase(char measurement[],int i2c_adrress, uint64_t measurment_time
     sprintf(header,"POST /write?db=%s&precision=ms&u=%s&p=%s HTTP/1.1\r\nHost: influx:8086\r\nContent-Length: %d\r\n\r\n", 
         DATABASE, USERNAME, PASSWORD, strlen(body));
 
-    //printf("Send to InfluxDB the POST request bytes=%d \n->|%s|<-\n",strlen(header), header);
-    ret = write(sockfd, header, strlen(header));
-    if (ret < 0)
-        pexit("Write Header request to InfluxDB failed");
+    uint8_t done = FAIL; 
+    uint8_t fail_count = 0;
+    while(done != SUCSESS){
 
-    if(DEBUG == 1){
-        printf("Send to InfluxDB the data bytes=%d \n->|%s|<-\n",strlen(body), body);
-    }
-    ret = write(sockfd, body, strlen(body));
-    if (ret < 0)
-        pexit("Write Data Body to InfluxDB failed");
+        if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) <0)
+            pexit("connect() failed");
 
-    // Get back the acknwledgement from InfluxDB //
-    // "HTTP/1.1 204 No Content" is the expectet server return string
-    ret = read(sockfd, result, sizeof(result));
-    if (ret < 0){
-        pexit("Reading the result from InfluxDB failed");
-    }
-    result[ret] = 0; // terminate string
-    if(DEBUG == 1){
-        printf("Result returned:%s\n",result);
-    }else{
-        if (result[9] == '2' && result[10] == '0' && result[11] == '4'){
-           // printf("  OK\n");
+        //printf("Send to InfluxDB the POST request bytes=%d \n->|%s|<-\n",strlen(header), header);
+        ret = write(sockfd, header, strlen(header));
+        if (ret < 0)
+            pexit("Write Header request to InfluxDB failed");
+
+        if(DEBUG == 1){
+            printf("Send to InfluxDB the data bytes=%d \n->|%s|<-\n",strlen(body), body);
         }
-        else{
-            printf("Error: %c%c%c\n",result[9],result[10],result[11]);
+        ret = write(sockfd, body, strlen(body));
+        if (ret < 0)
+            pexit("Write Data Body to InfluxDB failed");
+
+        // Get back the acknwledgement from InfluxDB //
+        // "HTTP/1.1 204 No Content" is the expectet server return string
+        ret = read(sockfd, result, sizeof(result));
+        if (ret < 0){
+            pexit("Reading the result from InfluxDB failed");
+        }
+        result[ret] = 0; // terminate string
+        if(DEBUG == 1){
+            printf("Result returned:%s\n",result);
+        }else{
+            if (result[9] == '2' && result[10] == '0' && result[11] == '4'){
+                done = SUCSESS;
+               // printf("  OK\n");
+            }
+            else{
+                printf("Error: %c%c%c\n",result[9],result[10],result[11]);
+                fail_count++;
+
+            }
+        }
+
+        if(close(sockfd) < 0){
+            pexit("close failed");
+        }
+        sleep(5*fail_count);
+        if (fail_count > 6)
+        {
             pexit("Write to Database Failed!");
         }
-    }
-
-
-    if(close(sockfd) < 0){
-        pexit("close failed");
     }
     return 0;
 }
