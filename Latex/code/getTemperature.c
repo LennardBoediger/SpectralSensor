4b44e843cb73b5584e318e6b6c97a5b749468b4c
Lennard <lennard@Lennards-MBP.fritz.box>
2021-03-12 20:02:48 +0100

    New easy to integrate 1.4 Release

--- Latex/code/getTemperature.c
+++
@@ -1,5 +0,0 @@
-//Returns the temperature in C
-//Pretty inaccurate: +/-8.5C //TODO: mabe include external Termometer to improve Readings
-uint8_t getTemperature(int fd){
-    return (virtualReadRegister(AS726x_DEVICE_TEMP, fd));
-}
