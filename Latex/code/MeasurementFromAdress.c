b289d961393d5d6742d5daff51f6af8780659001
Lennard <lennard@Lennards-MacBook-Pro.fritz.box>
2021-03-26 19:17:59 +0100

    add stuff from gitW
    #

--- Latex/code/MeasurementFromAdress.c
+++
@@ -1,9 +0,0 @@
-//Calls takeMeasurements on gives I2C Adrress
-void MeasurementFromAdress(int address){
-    int fd =  wiringPiI2CSetup(address);
-    if (fd == -1) {
-        printf("i2c failed");
-    }
-    takeMeasurements(fd);  // takesMeasurmant Readings can now be accessed via getX(), getY(), etc
-    close(fd);
-}
